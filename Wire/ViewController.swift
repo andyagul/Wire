fbc6c2aff928bc31eb21ec8b7ecb8caecdd637dd
Chen Weiru <506624047@qq.com>
2018-03-20 19:45:27 +0800

    Initial Commit

---
+++ Wire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wire
+//
+//  Created by Chen Weiru on 20/03/2018.
+//  Copyright © 2018 ChenWeiru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
